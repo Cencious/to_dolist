4a9530ea949788f1710f9d7120a5026a15755e99
Cencious <user.email>
2022-07-26 11:25:45 +0300

    query the method POST

--- task/views.py
+++ task/views.py
@@ -2,8 +2,15 @@ from contextlib import contextmanager
 from django.shortcuts import render
 # from django.http import HttpResponse
 from .models import *
+from .forms import *
 # Create your views here.
 def index(request):
     tasks = Task.objects.all()
-    context = {'tasks': tasks}
+
+    form = TaskForm()
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        
+    context = {'tasks': tasks, 'form': form}
     return render(request, 'task/list.html', context)
