4421c0565d93e8b07f9525c5b6e3476cfc4a330c
Cencious <user.email>
2022-08-03 12:58:04 +0300

    create a query for the update

--- task/views.py
+++ task/views.py
@@ -1,9 +1,11 @@
-from contextlib import contextmanager
 from django.shortcuts import render, redirect
-# from django.http import HttpResponse
+from django.http import HttpResponse
+
 from .models import *
 from .forms import *
+
 # Create your views here.
+
 def index(request):
     tasks = Task.objects.all()
 
@@ -22,6 +24,12 @@ def updateTask(request, pk):
     task=Task.objects.get(id=pk)
 
     form = TaskForm(instance=task)
-   
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST,instance=task)
+        if form.is_valid():
+            return redirect('/')
+
+    context = {'form': form}
 
     return render(request, 'task/update_task.html', context)
