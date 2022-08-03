99879187f8974d87302b63e39b776dfdbb4475d7
Cencious <user.email>
2022-08-03 22:31:04 +0300

    query the delete functionality

--- task/views.py
+++ task/views.py
@@ -36,5 +36,11 @@ def updateTask(request, pk):
     return render(request, 'task/update_task.html', context)
 
 def deleteTask(request, pk):
-    item = Task.objects.get(pk=pk)
-    return render(request, 'task/delete_task.html')
+    item = Task.objects.get(id=pk)
+
+    if request.method == 'POST':
+       item.delete()
+       return redirect('/') 
+       
+    context = {'item': item}
+    return render(request, 'task/delete.html', context)
